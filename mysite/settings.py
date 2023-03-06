f62d4e2c55ef512e4a9884799bb20155717e4c7b
OscarSoleye <clouddavinci@gmail.com>
2023-03-06 14:34:36 +0000

    installed faker

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'temp_stories.apps.TempStoriesConfig',
 ]
 
 MIDDLEWARE = [
